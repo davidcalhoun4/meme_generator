79349c91cdd125038df65585e373e54c6cfbf1df
david <calhoun5321@gmail.com>
2021-07-01 19:22:27 -0700

    removes comments on MemeGenerator.js, updates header image

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -63,12 +63,6 @@ class MemeGenerator extends Component {
     alert("saved");
   }
 
-  /**
-   * Create a method that, when the "Gen" button is clicked, chooses one of the
-   * memes from our `allMemeImgs` array at random and makes it so that is the
-   * meme image that shows up in the bottom portion of our meme generator site (`.url`)
-   */
-
   render() {
     return (
       <div>
